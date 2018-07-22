495d2f1eaa245d9698defc6755756fd042f6221d
Rob Hardt <rhardt@pivotal.io>
2018-07-22 17:30:44 -0400

    init

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+set -x
+output=$(echo "ping" | nc ${TCP_APP_HOST} ${TCP_APP_PORT})
+if [[ "$output" == "${EXPECTED_RESPONSE}" ]]; then
+  echo "received \"${output}\" as expected"
+  exit 0
+else
+  echo "unexpected response: \"${output}\""
+  exit 1
+fi
